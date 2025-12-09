af49e421bf1cf6e341301d725b64462d207b4b7b
atf-korea <atf.kr@atfconsulting.org>
2025-12-09 14:00:50 +0900

    Delete spk/config_spk.js

--- spk/config_spk.js
+++
@@ -1,5 +0,0 @@
-// config.js
-const GOOGLE_API_KEY = 'AIzaSyBS1kIHPn1j2iCL4npiitSo_6Sm0M42450';
-const GOOGLE_SHEET_ID = '1kmYORT3qYSl1dwCWA-afPFXw6eKLYlT9c92mpC44PgI';
-const SHEET_RANGE = 'SPK_Site!A1:G'; 
-
