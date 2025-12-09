92d745da814b5120da1739bb48429c8b224d0ab6
atf-korea <atf.kr@atfconsulting.org>
2025-12-09 14:01:15 +0900

    Update config.js

--- forcass/config.js
+++ forcass/config.js
@@ -1,4 +1,3 @@
 // config.js
-const GOOGLE_API_KEY = 'AIzaSyBS1kIHPn1j2iCL4npiitSo_6Sm0M42450';
-const GOOGLE_SHEET_ID = '1CtvvJmwuEw1LA9GjGe1RncKMTdECQxwU-CVFkj122iA';
-const SHEET_RANGE = 'Tax_Hub!A1:E'; 
+
+
